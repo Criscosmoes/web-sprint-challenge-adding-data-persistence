97f96c7faeba1e7b550555e08259bf9a7ea56c15
Cristian Fernandez <cristian.fernandez123@yahoo.com>
2020-12-11 14:44:03 -0800

    fixed up some queries, everything is now working!

--- api/task/model.js
+++ api/task/model.js
@@ -10,6 +10,8 @@ module.exports = {
           })
     }, 
     getAll(){
-        return db('tasks'); 
+        return db('tasks as t')
+        .join("projects as p", "t.task_id" ,"p.project_id")
+        .select("task_id", "task_description", "task_notes", "t.completed", "p.project_name", "p.description")
     }
 }
